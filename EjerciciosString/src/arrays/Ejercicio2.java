df6166a657cd1c90359fd3e273ea16cf646419dd
Dsaezfaus <dam.dsaez@institutoserpis.org>
2015-11-20 13:04:32 +0100

    Ejercicios arrays

--- EjerciciosString/src/arrays/Ejercicio2.java
+++ EjerciciosString/src/arrays/Ejercicio2.java
@@ -8,10 +8,14 @@ Repetir para los de posición impar.
 public class Ejercicio2 {
     public static void main (String args[]){
         Scanner tec = new Scanner (System.in);
-        Random rn=new Random();
         String [] vector=new String[10];
+        System.out.println("Introduzca una cadena de carácteres alfanuméricos");
         for(int i=0;i<vector.length;i++){
-            if(i%2==0)
+            vector[i]=tec.next();
+        }
+        
+        for(int i=0;i<vector.length;i++){
+        if(i%2==0)
             System.out.println("El numero de la posicion "+i+" es "+vector[i]+" que es par");
             else System.out.println("El numero de la posicion "+i+" es "+vector[i]+" que es impar");
         }
